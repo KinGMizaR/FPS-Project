7c60fcd44e5fac7d14e53dc21f34196741f0db09
Cristian Camilo Palacio <cristianc57@hotmail.es>
2019-07-06 21:51:43 -0500

    Correccion

---
+++ FPSProject/Assets/Camarita.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camarita : MonoBehaviour
+{
+    public float mouseX ;
+    public float mouseY ;
+    public bool invertedAxis;
+    public float speed;
+
+    void Update()
+    {    
+        //Aqui obtenemos la posicion del Mouse tanto en el eje Y como en el X y la vinculamos a la camara 
+        mouseX += Input.GetAxis("Mouse X");
+        if (invertedAxis)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+        }
+        else
+        {
+            mouseY -= Input.GetAxis("Mouse Y");
+        }
+
+        // aqui se ajusta la velocidad de la camara de forma manual
+        Debug.Log(mouseX);
+        transform.eulerAngles = new Vector3(mouseY*speed, mouseX*speed, 0);
+    }
+}
---
+++ FPSProject/Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Camarita rt;
+    public float speed =0.1f;
+    void Update()
+    {   
+        //Aqui asignamos el movimiento del personaje a las teclas WASD 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward*speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+        //Aqui se asigna la rotacion del eje Y del personaje segun el eje X de la camara  
+        transform.eulerAngles = new Vector3(0, rt.mouseX * rt.speed, 0);
+
+    }
+}
